4390f6cf7e4125a81464c3bcdc5b98fdd0cd5e3b
Erik Grubbs <erikgrubbs@gmail.com>
2019-09-26 00:49:14 -0700

    completes Step 3

--- database/seed.js
+++ database/seed.js
@@ -1,1 +1,29 @@
 // TODO
+const db = require('./index.js');
+
+const restaurants = [
+  {
+    name: "Howlin Rays",
+    rating: 5
+  },
+  {
+    name: "Mariscos Jalisco",
+    rating: 5
+  },
+  {
+    name: "Hummus Factory",
+    rating: 4
+  },
+  {
+    name: "Joy",
+    rating: 4
+  },
+  {
+    name: "Rustic Canyon",
+    rating: 5
+  }
+];
+
+restaurants.forEach(({name, rating}) => {
+  db.query(`INSERT INTO restaurants (restaurant_name, rating) VALUES ("${name}",${rating});`)
+})
